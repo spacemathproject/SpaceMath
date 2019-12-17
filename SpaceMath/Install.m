25230c2febe36c6d1ad5e9218b2007a9a2b881e2
valencia <valencia@adrii>
2019-12-17 15:22:03 -0600

    update

--- SpaceMath/Install.m
+++ SpaceMath/Install.m
@@ -52,12 +52,12 @@ InstallSpaceMath[OptionsPattern[]]:=
 				SMgetUrl, configFileProlog,
 				OverwriteSM, zipDir,
 				useTraditionalForm},
-(*
+
 	If[OptionValue[InstallSpaceMathDevelopmentVersion],
 		gitzip = OptionValue[SpaceMathDevelopmentVersionLink],
 		gitzip = OptionValue[SpaceMathStableVersionLink]
 	];
-*)
+
 	useTraditionalForm=True;
 
 	packageName = "SpaceMath";
