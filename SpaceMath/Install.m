d0e6657d601a9ebd989d9fb93a52f0dc538a8851
valencia <valencia@adrii>
2019-12-19 13:16:17 -0600

    update

--- SpaceMath/Install.m
+++ SpaceMath/Install.m
@@ -5,16 +5,6 @@ InstallSpaceMath::notcomp =
 InstallSpaceMath::failed =
 "Download of `1` failed. Installation aborted!";
 
-AutoOverwriteSpaceMathDirectory::usage="AutoOverwriteSpaceMathDirectory is an option of InstallSpaceMath. If \
-set to True, the existing SpaceMath directory will be deleted without any further notice. The default
-value None means that the user will be asked by a dialog. False means that the directory will be overwritten.";
-
-SpaceMathStableVersionLink::usage="SpaceMathStableVersionLink is an option of InstallSpaceMath. It specifies the url \
-to the latest stable release of SpaceMath.";
-
-InstallSpaceMathTo::usage="InstallSpaceMathTo is an option of InstallSpaceMath. It specifies, the full path \
-to the directory where SpaceMath will be installed.";
-
 $PathToSPArc::usage="$PathToSPArc specifies where the installer should look for the zipped SpaceMath version. \
 If the value is not empty, the installer will use the specified file instead of downloading it from the official \
 website."
@@ -23,19 +13,6 @@ If[ !ValueQ[$PathToSPArc],
 	$PathToSPArc = ""
 ];
 
-(*
-If[  $VersionNumber == 8,
-Needs["Utilities`URLTools`"];
-];
-*)
-
-(*
-Options[InstallSpaceMath]={
-	AutoOverwriteSpaceMathDirectory-> None,
-	SpaceMathStableVersionLink->"https://github.com/spacemathproject/SpaceMath/archive/developerTAVP.zip",
-	InstallSpaceMathTo->FileNameJoin[{$UserBaseDirectory, "Applications","SpaceMath"}]
-};
-*)
 (*	
 InstallSpaceMath[OptionsPattern[]]:=
 *)
@@ -90,28 +67,12 @@ zipDir = FileNames["SpaceMath.m", unzipDir, Infinity];
 
 		fullPath = DirectoryName[zipDir[[1]]];
 		zipDir = Last[FileNameSplit[DirectoryName[zipDir[[1]]]]];
-(**
-	If[ Length[zipDir]===1,
-		fullPath = DirectoryName[zipDir[[1]]];
-		zipDir = Last[FileNameSplit[DirectoryName[zipDir[[1]]]]];
-		WriteString["stdout", "done! \n"],
-		WriteString["stdout", "\nFailed to recognize the directory structure of the downloaded zip file. \nInstallation aborted!"];
-		Abort[]
-	];
-*)
+
 (* Move the files to the final destination	*)
 WriteString["stdout", "Copying "<>packageName<>" to ", packageDir, " ..."];
 CopyDirectory[fullPath,packageDir];
 Quiet@DeleteDirectory[unzipDir, DeleteContents -> True];
-(*
-	If[	CopyDirectory[fullPath,packageDir]===$Failed,
-		WriteString["stdout", "\nFailed to copy "  <>fullPath<>" to ", packageDir <>". \nInstallation aborted!"];
-		Abort[],
-		WriteString["stdout", "done! \n"];
-		(* Delete the extracted archive *)
-		Quiet@DeleteDirectory[unzipDir, DeleteContents -> True];
-	];
-*)
+
 WriteString["stdout", "done! \n"];
 WriteString["stdout", "\nInstallation complete! Loading SpaceMath ... \n"];
 Get["SpaceMath`"];
