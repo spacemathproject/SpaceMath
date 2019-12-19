024f486ac8b4102dd66284e6cb150faba1589b40
valencia <valencia@adrii>
2019-12-19 13:57:22 -0600

    update

--- SpaceMath/Install.m
+++ SpaceMath/Install.m
@@ -24,46 +24,32 @@ InstallSpaceMath[]:=
 
 			SMgetUrl[x_]:= URLSave[x,CreateTemporary[]];
 
-	(* If the package directory already exists, ask the user about overwriting *)
-	If[ DirectoryQ[packageDir],
+			(* If the package directory already exists, ask the user about overwriting *)
+			If[ DirectoryQ[packageDir],
+				If[ None, Quiet@DeleteDirectory[packageDir, DeleteContents -> True], Null,
+					If[ ChoiceDialog[OverwriteSM,{"Yes, overwrite the " <> packageName <>" directory"->True,
+						"No, I need to do a backup first. Abort installation."->False}, WindowFloating->True, WindowTitle->"Existing SpaceMath Installation detected"],
+						Quiet@DeleteDirectory[packageDir, DeleteContents -> True],
+						Abort[]
+					  ]
+				  ]
+			  ];
 
-(*		If[ OptionValue[AutoOverwriteSpaceMathDirectory], *)
-		If[ None,
-			Quiet@DeleteDirectory[packageDir, DeleteContents -> True],
-			Null,
-			If[ ChoiceDialog[OverwriteSM,{"Yes, overwrite the " <> packageName <>" directory"->True,
-				"No, I need to do a backup first. Abort installation."->False}, WindowFloating->True, WindowTitle->"Existing SpaceMath Installation detected"],
-				Quiet@DeleteDirectory[packageDir, DeleteContents -> True],
-				Abort[]
-			]
-		]
-	];
+			tmpzip = $PathToSPArc;
+			tmpzip=SMgetUrl[gitzip];
+			unzipDir= tmpzip<>".dir";
 
-tmpzip = $PathToSPArc;
-WriteString["stdout", "Installing SpaceMath from ", tmpzip," ..."];
-WriteString["stdout", "Downloading SpaceMath from ", gitzip," ..."];
-tmpzip=SMgetUrl[gitzip];
+			ExtractArchive[tmpzip, unzipDir];
+			Quiet@DeleteFile[tmpzip];
 
-unzipDir= tmpzip<>".dir";
-WriteString["stdout", "SpaceMath zip file was saved to ", tmpzip,".\n"];
-WriteString["stdout", "Extracting SpaceMath zip file to ", unzipDir, " ..."];
+			zipDir = FileNames["SpaceMath.m", unzipDir, Infinity];
+			fullPath = DirectoryName[zipDir[[1]]];
+			zipDir = Last[FileNameSplit[DirectoryName[zipDir[[1]]]]];
 
-ExtractArchive[tmpzip, unzipDir];
-Quiet@DeleteFile[tmpzip];
+			CopyDirectory[fullPath,packageDir];
+			Quiet@DeleteDirectory[unzipDir, DeleteContents -> True];
 
-WriteString["stdout", "Recognizing the directory structure..."];
-zipDir = FileNames["SpaceMath.m", unzipDir, Infinity];
-
-		fullPath = DirectoryName[zipDir[[1]]];
-		zipDir = Last[FileNameSplit[DirectoryName[zipDir[[1]]]]];
-
-(* Move the files to the final destination	*)
-WriteString["stdout", "Copying "<>packageName<>" to ", packageDir, " ..."];
-CopyDirectory[fullPath,packageDir];
-Quiet@DeleteDirectory[unzipDir, DeleteContents -> True];
-
-WriteString["stdout", "done! \n"];
-WriteString["stdout", "\nInstallation complete! Loading SpaceMath ... \n"];
-Get["SpaceMath`"];
+			WriteString["stdout", "\nInstallation complete! Loading SpaceMath ... \n"];
+			Get["SpaceMath`"];
 
 ];
