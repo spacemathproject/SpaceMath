3b2b1dfbc209e57d2ef12e7ed7f8ceb8951afc62
valencia <valencia@adrii>
2019-12-22 13:18:32 -0600

    update

--- SpaceMath/Install.m
+++ SpaceMath/Install.m
@@ -2,37 +2,38 @@
 
 InstallSpaceMath[]:=
 		Module[
-	     	{	unzipDir, tmpzip, packageName, packageDir, fullPath, OverwriteSM, zipDir
+	     	{ 
+	     	 DecompressTempFolder, TempCompressFolder, packageName, PackageLocation, fullPath, OverwritePackage, zipDir
 			},
         
 			packageName = "SpaceMath";
-			packageDir = FileNameJoin[{$UserBaseDirectory, "Applications","SpaceMath"}];			
+			PackageLocation = FileNameJoin[{$UserBaseDirectory, "Applications","SpaceMath"}];			
 
-			OverwriteSM="Looks like SpaceMath is already installed. Do you want to replace the content \
-			of " <> packageDir <> " with the downloaded version of SpaceMath?";
+			OverwritePackage="Looks like SpaceMath is already installed. Do you want to replace the content \
+			of " <> PackageLocation <> " with the downloaded version of SpaceMath?";
 
-			If[ DirectoryQ[packageDir],
-				If[ None, Quiet@DeleteDirectory[packageDir, DeleteContents -> True], Null,
-					If[ ChoiceDialog[OverwriteSM,{"Yes, overwrite the " <> packageName <>" directory"->True,
+			If[ DirectoryQ[PackageLocation],
+				If[ None, Quiet@DeleteDirectory[PackageLocation, DeleteContents -> True], Null,
+					If[ ChoiceDialog[OverwritePackage,{"Yes, overwrite the " <> packageName <>" directory"->True,
 						"No, I need to do a backup first. Abort installation."->False}, WindowFloating->True, WindowTitle->"Existing SpaceMath Installation detected"],
-						Quiet@DeleteDirectory[packageDir, DeleteContents -> True],
+						Quiet@DeleteDirectory[PackageLocation, DeleteContents -> True],
 						Abort[]
 					  ]
 				  ]
 			  ];
 
-			tmpzip = URLSave["https://github.com/spacemathproject/SpaceMath/archive/developerTAVP.zip",CreateTemporary[]];
-			unzipDir= tmpzip<>".dir";
+			TempCompressFolder = URLSave["https://github.com/spacemathproject/SpaceMath/archive/developerTAVP.zip",CreateTemporary[]];
+			DecompressTempFolder= TempCompressFolder<>".dir";
 
-			ExtractArchive[tmpzip, unzipDir];
-			Quiet@DeleteFile[tmpzip];
+			ExtractArchive[TempCompressFolder, DecompressTempFolder];
+			Quiet@DeleteFile[TempCompressFolder];
 
-			zipDir = FileNames["SpaceMath.m", unzipDir, Infinity];
+			zipDir = FileNames["SpaceMath.m", DecompressTempFolder, Infinity];
 			fullPath = DirectoryName[zipDir[[1]]];
 			zipDir = Last[FileNameSplit[DirectoryName[zipDir[[1]]]]];
 
-			CopyDirectory[fullPath,packageDir];
-			Quiet@DeleteDirectory[unzipDir, DeleteContents -> True];
+			CopyDirectory[fullPath,PackageLocation];
+			Quiet@DeleteDirectory[DecompressTempFolder, DeleteContents -> True];
 
 			WriteString["stdout", "\nInstallation complete! Loading SpaceMath ... \n"];
 			Get["SpaceMath`"];
